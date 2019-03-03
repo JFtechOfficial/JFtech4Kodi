a9af6f8e2f6a73548ba8f60b4186e6238c248466
JF <gianlucagalli@MacBook-Pro-di-Gianluca-4.local>
2019-03-03 16:08:21 +0100

    Repo rework and addon update

--- plugin.program.hyperion-controller/default.py
+++ plugin.program.hyperion-controller/default.py
@@ -1,44 +1,39 @@
-import sys
+
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
-import xbmcvfs
-import urllib
-import urlparse
-import json
+import simplejson as json
 import hyperion_client
 import pyxbmct
-import os
 import socket
 import routing
 
-#base_url = sys.argv[0]
-#addon_handle = int(sys.argv[1])
-#args = urlparse.parse_qs(sys.argv[2][1:])
-
-
 ADDON = xbmcaddon.Addon()
 #ADDONNAME = ADDON.getAddonInfo('id')
-
-##mode = args.get('mode', None)
-##divider = '-'
 
 plugin = routing.Plugin()
 
 xbmcplugin.setContent(plugin.handle, 'files')
 
-color_path = xbmc.translatePath('special://home/addons/plugin.program.hyperion-controller/colors.json')
+#path = os.path.dirname(os.path.realpath(__file__))
+#color_path = os.path.join(xbmc.translatePath(path), "colors.json")
+color_path = xbmc.translatePath(
+    'special://home/addons/plugin.program.hyperion-controller/colors.json')
 ip = str(ADDON.getSetting('ip'))
 port = ADDON.getSetting('port')
-apriority = ADDON.getSetting('priority') 
+apriority = ADDON.getSetting('priority')
 h = hyperion_client.hyperion_client(ip, port)
+debug = ADDON.getSetting('debug')
+if debug:
+    pass
 
-        
+
 def translate(text):
     return ADDON.getLocalizedString(text).encode("utf-8")
 
 ##################################
-    
+
+
 class MyAddon(pyxbmct.AddonDialogWindow):
 
     def __init__(self, title=''):
@@ -60,7 +55,7 @@ class MyAddon(pyxbmct.AddonDialogWindow):
                                pyxbmct.ACTION_MOUSE_DRAG,
                                pyxbmct.ACTION_MOUSE_LEFT_CLICK],
                               self.slider_update)
-        
+
         self.okButton = pyxbmct.Button('Ok')
         self.placeControl(self.okButton, 3, 2)
         self.connect(self.okButton, self.launch)
@@ -74,7 +69,8 @@ class MyAddon(pyxbmct.AddonDialogWindow):
     def set_sliderR(self):
         # Slider value label
         SLIDER_INIT_VALUE = self.active_RGB[0]
-        self.slider_valueR = pyxbmct.Label('[COLOR FFFF0000]' + str(SLIDER_INIT_VALUE) + '[/COLOR]', alignment=pyxbmct.ALIGN_CENTER)
+        self.slider_valueR = pyxbmct.Label(
+            '[COLOR FFFF0000]' + str(SLIDER_INIT_VALUE) + '[/COLOR]', alignment=pyxbmct.ALIGN_CENTER)
         self.placeControl(self.slider_valueR, 0, 1)
         #
         slider_caption = pyxbmct.Label(translate(30007), alignment=pyxbmct.ALIGN_CENTER)
@@ -82,14 +78,14 @@ class MyAddon(pyxbmct.AddonDialogWindow):
         # Slider
         self.sliderR = pyxbmct.Slider()
         self.placeControl(self.sliderR, 0, 2, pad_y=10, columnspan=2)
-        #self.slider.setPercent(SLIDER_INIT_VALUE)
+        # self.slider.setPercent(SLIDER_INIT_VALUE)
         self.sliderR.setInt(SLIDER_INIT_VALUE, -10, 5, 255)
-
 
     def set_sliderG(self):
         # Slider value label
         SLIDER_INIT_VALUE = self.active_RGB[1]
-        self.slider_valueG = pyxbmct.Label('[COLOR FF00FF00]' + str(SLIDER_INIT_VALUE) + '[/COLOR]', alignment=pyxbmct.ALIGN_CENTER)
+        self.slider_valueG = pyxbmct.Label(
+            '[COLOR FF00FF00]' + str(SLIDER_INIT_VALUE) + '[/COLOR]', alignment=pyxbmct.ALIGN_CENTER)
         self.placeControl(self.slider_valueG, 1, 1)
         #
         slider_caption = pyxbmct.Label(translate(30008), alignment=pyxbmct.ALIGN_CENTER)
@@ -97,14 +93,14 @@ class MyAddon(pyxbmct.AddonDialogWindow):
         # Slider
         self.sliderG = pyxbmct.Slider()
         self.placeControl(self.sliderG, 1, 2, pad_y=10, columnspan=2)
-        #self.slider.setPercent(SLIDER_INIT_VALUE)
+        # self.slider.setPercent(SLIDER_INIT_VALUE)
         self.sliderG.setInt(SLIDER_INIT_VALUE, -10, 5, 255)
-
 
     def set_sliderB(self):
         # Slider value label
         SLIDER_INIT_VALUE = self.active_RGB[2]
-        self.slider_valueB = pyxbmct.Label('[COLOR FF0000FF]' + str(SLIDER_INIT_VALUE) + '[/COLOR]', alignment=pyxbmct.ALIGN_CENTER)
+        self.slider_valueB = pyxbmct.Label(
+            '[COLOR FF0000FF]' + str(SLIDER_INIT_VALUE) + '[/COLOR]', alignment=pyxbmct.ALIGN_CENTER)
         self.placeControl(self.slider_valueB, 2, 1)
         #
         slider_caption = pyxbmct.Label(translate(30009), alignment=pyxbmct.ALIGN_CENTER)
@@ -112,9 +108,8 @@ class MyAddon(pyxbmct.AddonDialogWindow):
         # Slider
         self.sliderB = pyxbmct.Slider()
         self.placeControl(self.sliderB, 2, 2, pad_y=10, columnspan=2)
-        #self.slider.setPercent(SLIDER_INIT_VALUE)
+        # self.slider.setPercent(SLIDER_INIT_VALUE)
         self.sliderB.setInt(SLIDER_INIT_VALUE, -10, 5, 255)
-    
 
     def launch(self):
         red = self.sliderR.getInt()
@@ -123,13 +118,13 @@ class MyAddon(pyxbmct.AddonDialogWindow):
         try:
             global h
             h.set_RGBcolor(red=red, green=green, blue=blue, priority=apriority)
-            line = translate(30010) + str(red) + ', ' + str(green) + ', ' + str(blue) + ' on Hyperion ' + ip
+            line = translate(30010) + str(red) + ', ' + str(green) + \
+                ', ' + str(blue) + ' on Hyperion ' + ip
             xbmcgui.Dialog().notification(translate(30003), line)
             h.close_connection()
         except socket.error:
             xbmcgui.Dialog().notification(translate(30012), translate(30013), xbmcgui.NOTIFICATION_ERROR)
         self.close()
-
 
     def set_navigation(self):
         # Set navigation between controls
@@ -166,11 +161,10 @@ class MyAddon(pyxbmct.AddonDialogWindow):
         except (RuntimeError, SystemError):
             pass
 
-
     def setAnimation(self, control):
         # Set fade animation for all add-on window controls
         control.setAnimations([('WindowOpen', 'effect=fade start=0 end=100 time=200',),
-                                ('WindowClose', 'effect=fade start=100 end=0 time=200',)])
+                               ('WindowClose', 'effect=fade start=100 end=0 time=200',)])
 
 ##################################
 
@@ -178,45 +172,50 @@ class MyAddon(pyxbmct.AddonDialogWindow):
 @plugin.route('/')
 def index():
     li = xbmcgui.ListItem(translate(30000))
-    image='https://i.imgur.com/WowKBZT.png'#'http://www.weareclear.co.uk/wp-content/uploads/2017/12/logo.png'
+    # 'http://www.weareclear.co.uk/wp-content/uploads/2017/12/logo.png'
+    image = 'https://i.imgur.com/WowKBZT.png'
     li.setArt({'thumb': image,
-                'icon': image})
-    xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(clear), listitem=li, isFolder=False)
+               'icon': image})
+    xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(
+        clear), listitem=li, isFolder=False)
 
     li = xbmcgui.ListItem(translate(30001))
-    image='https://imgur.com/2k0E5R1.png'#'https://teetribe.eu/wp-content/uploads/2018/05/RGB-Red-Green-Blue.png'
+    # 'https://teetribe.eu/wp-content/uploads/2018/05/RGB-Red-Green-Blue.png'
+    image = 'https://imgur.com/2k0E5R1.png'
     li.setArt({'thumb': image,
-                'icon': image,
-              'fanart': image})
+               'icon': image,
+               'fanart': image})
     xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(colors),
                                 listitem=li, isFolder=True)
 
     li = xbmcgui.ListItem(translate(30002))
-    image='https://png.icons8.com/color/1600/color-wheel-2.png'
+    image = 'https://png.icons8.com/color/1600/color-wheel-2.png'
     li.setArt({'thumb': image,
-                'icon': image,
-              'fanart': image})
+               'icon': image,
+               'fanart': image})
     xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(effects),
                                 listitem=li, isFolder=True)
 
     li = xbmcgui.ListItem(translate(30003))
-    image='https://imgur.com/DdRsSe9.png'#'https://imgur.com/2HxFrHl.png'
-    #'https://sites.google.com/site/makecolorsimages/sliders-RGB_512x512.png'
+    image = 'https://imgur.com/DdRsSe9.png'
+    # 'https://sites.google.com/site/makecolorsimages/sliders-RGB_512x512.png'
     li.setArt({'thumb': image,
-                'icon': image})
+               'icon': image})
     xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(RGB_sliders), listitem=li)
 
     li = xbmcgui.ListItem(translate(30004))
-    image='https://cdn4.iconfinder.com/data/icons/meBaze-Freebies/512/setting.png'
+    image = 'https://cdn4.iconfinder.com/data/icons/meBaze-Freebies/512/setting.png'
     li.setArt({'thumb': image,
-                'icon': image})
-    xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(settings), listitem=li, isFolder=False)
- 
+               'icon': image})
+    xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(
+        settings), listitem=li, isFolder=False)
+
     li = xbmcgui.ListItem(translate(30005))
-    image='https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fko-fi.com%2FY8Y0FW3V&chs=180x180&choe=UTF-8&chld=L|2'
+    image = 'https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fko-fi.com%2FY8Y0FW3V&chs=180x180&choe=UTF-8&chld=L|2'
     li.setArt({'thumb': image,
-                'icon': image})
-    xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(donate), listitem=li, isFolder=False)
+               'icon': image})
+    xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(
+        donate), listitem=li, isFolder=False)
 
     xbmcplugin.endOfDirectory(plugin.handle)
 
@@ -225,7 +224,7 @@ def index():
 def clear():
     try:
         h.clear_all()
-        xbmcgui.Dialog().notification(translate(30000), translate(30005))
+        xbmcgui.Dialog().notification(translate(30000), translate(30006))
         h.close_connection()
     except socket.error:
         xbmcgui.Dialog().notification(translate(30012), translate(30013), xbmcgui.NOTIFICATION_ERROR)
@@ -234,15 +233,16 @@ def clear():
 @plugin.route('/colors')
 def colors():
     with open(color_path) as f:
-        colornames = json.load(f)            
+        colornames = json.load(f)
     for color in colornames:
         hexColor = '%02x%02x%02x' % tuple(colornames[color])
         img = 'https://dummyimage.com/100x100/' + hexColor + '/' + hexColor + '.jpg'
         li = xbmcgui.ListItem(color)
         li.setArt({'thumb': img,
-                    'icon': img,
-                    'fanart': img})       
-        xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(color_launcher, color), listitem=li, isFolder=False)
+                   'icon': img,
+                   'fanart': img})
+        xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(
+            color_launcher, color), listitem=li, isFolder=False)
     xbmcplugin.endOfDirectory(plugin.handle)
 
 
@@ -251,7 +251,8 @@ def color_launcher(color):
     with open(color_path) as f:
         colornames = json.load(f)
     try:
-        h.set_RGBcolor(red=colornames[color][0],green=colornames[color][1],blue=colornames[color][2], priority=apriority)
+        h.set_RGBcolor(red=colornames[color][0], green=colornames[color]
+                       [1], blue=colornames[color][2], priority=apriority)
         line = translate(30010) + color + translate(30011) + ip
         xbmcgui.Dialog().notification(translate(30001), line)
         h.close_connection()
@@ -264,9 +265,9 @@ def effects():
     try:
         effectnames = h.effects_names()
         for effect in effectnames:
-            #url = 'http://www.vidsplay.com/wp-content/uploads/2017/04/alligator.mp4'
             li = xbmcgui.ListItem(effect)
-            xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(effect_launcher, effects), listitem=li)
+            xbmcplugin.addDirectoryItem(handle=plugin.handle, url=plugin.url_for(
+                effect_launcher, effect), listitem=li)
     except socket.error:
         xbmcgui.Dialog().notification(translate(30012), translate(30013), xbmcgui.NOTIFICATION_ERROR)
     xbmcplugin.endOfDirectory(plugin.handle)
@@ -304,148 +305,3 @@ def donate():
 
 if __name__ == '__main__':
     plugin.run()
-
-"""
-
-def build_url(query):
-    return base_url + '?' + urllib.urlencode(query)
-
-
-def launching(foldersplit):
-    if len(foldersplit) == 2:
-        name = foldersplit[1]
-    else:
-        name1 = ''
-        for i in range(1,len(foldersplit)):
-            name1 += foldersplit[i] + divider
-        name = name1[:-1]
-    return name
-
-
-if mode is None:
-    url = build_url({'mode': 'folder', 'foldername': 'Clear All'})
-    li = xbmcgui.ListItem('Clear all effects/color')
-    image='https://i.imgur.com/WowKBZT.png'#'http://www.weareclear.co.uk/wp-content/uploads/2017/12/logo.png'
-    li.setArt({'thumb': image,
-                'icon': image})
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-    
-    url = build_url({'mode': 'folder', 'foldername': 'Color'})
-    li = xbmcgui.ListItem('Color')
-    image='https://imgur.com/2k0E5R1.png'#'https://teetribe.eu/wp-content/uploads/2018/05/RGB-Red-Green-Blue.png'
-    li.setArt({'thumb': image,
-                'icon': image,
-              'fanart': image})
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
-                                listitem=li, isFolder=True)
-
-    url = build_url({'mode': 'folder', 'foldername': 'Effects'})
-    li = xbmcgui.ListItem('Effects')
-    image='https://png.icons8.com/color/1600/color-wheel-2.png'
-    li.setArt({'thumb': image,
-                'icon': image,
-              'fanart': image})
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
-                                listitem=li, isFolder=True)
-    sliders = 'RGB Sliders'
-    url = build_url({'mode': 'folder', 'foldername': sliders})
-    li = xbmcgui.ListItem(sliders)
-    image='https://imgur.com/2HxFrHl.png'#'https://sites.google.com/site/makecolorsimages/sliders-RGB_512x512.png'
-    li.setArt({'thumb': image,
-                'icon': image})
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-    
-    Settings = 'Settings'
-    url = build_url({'mode': 'folder', 'foldername': Settings})
-    li = xbmcgui.ListItem(Settings)
-    image='https://cdn4.iconfinder.com/data/icons/meBaze-Freebies/512/setting.png'
-    li.setArt({'thumb': image,
-                'icon': image})
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-
-    Donate = 'Support me here: https://ko-fi.com/Y8Y0FW3V '
-    url = build_url({'mode': 'folder', 'foldername': Donate})
-    li = xbmcgui.ListItem(Donate)
-    image='https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fko-fi.com%2FY8Y0FW3V&chs=180x180&choe=UTF-8&chld=L|2'
-    li.setArt({'thumb': image,
-                'icon': image})
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-    
-    #xbmcplugin.endOfDirectory(addon_handle)
-
-elif mode[0] == 'folder':
-    foldername = args['foldername'][0]
-
-    if foldername == 'Clear All':
-        try:
-            h.clear_all()
-            line = 'clearing...'
-            xbmcgui.Dialog().notification(foldername, line)
-            h.close_connection()
-        except socket.error:
-            xbmcgui.Dialog().notification(translate(30012), translate(30013), xbmcgui.NOTIFICATION_ERROR)
-
-    
-    foldersplit = foldername.split(divider)
-    if foldersplit[0] == 'Effects':
-        if foldername == 'Effects':
-            try:
-                effectnames =  h.effects_names()#['Snake', 'Rainbow swirl fast']
-                for e in effectnames:
-                    #url = 'http://www.vidsplay.com/wp-content/uploads/2017/04/alligator.mp4'
-                    url = build_url({'mode': 'folder', 'foldername': foldername + divider + e })
-                    li = xbmcgui.ListItem(e)
-                    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-            except socket.error:
-                xbmcgui.Dialog().notification(translate(30012), translate(30013), xbmcgui.NOTIFICATION_ERROR)
-
-
-        elif len(foldersplit) > 1:
-            nome = launching(foldersplit)
-            try:
-                h.set_effect(effectName=nome, priority=apriority)
-                line = translate(30010) + nome + translate(30011) + ip
-                xbmcgui.Dialog().notification(foldersplit[0], line)
-                h.close_connection()
-            except socket.timeout:
-                xbmcgui.Dialog().notification(translate(30012), translate(30013), xbmcgui.NOTIFICATION_ERROR)
-
-    elif foldersplit[0] == 'Color':
-        with open(color_path) as f:
-            colornames = json.load(f)
-        
-        if foldername == 'Color':            
-            for c in colornames:
-                url = build_url({'mode': 'folder', 'foldername': foldername + divider + c})
-                hexColor = '%02x%02x%02x' % tuple(colornames[c])
-                img = 'https://dummyimage.com/100x100/' + hexColor + '/' + hexColor + '.jpg'
-                li = xbmcgui.ListItem(c)
-                li.setArt({'thumb': img,
-                            'icon': img,
-                            'fanart': img})       
-                xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
-            #xbmcplugin.endOfDirectory(addon_handle)
-
-        elif len(foldersplit) > 1:
-            nome = launching(foldersplit)
-            try:
-                h.set_RGBcolor(red=colornames[nome][0],green=colornames[nome][1],blue=colornames[nome][2], priority=apriority)
-                line = translate(30010) + nome + translate(30011) + ip
-                xbmcgui.Dialog().notification(foldersplit[0], line)
-                h.close_connection()
-            except socket.error:
-                xbmcgui.Dialog().notification(translate(30012), translate(30013), xbmcgui.NOTIFICATION_ERROR)
-
-    elif foldername == 'RGB Sliders':
-        #xbmcgui.Dialog().ok(foldername, 'apre un control panel con 3 sliders')
-        window = MyAddon(foldername)
-        window.doModal()
-        # Destroy the instance explicitly because
-        # underlying xbmcgui classes are not garbage-collected on exit.
-        del window
-
-    elif foldername == 'Settings':
-        ADDON.openSettings()
-    
-xbmcplugin.endOfDirectory(addon_handle)
-"""
